98e610f4e0f3cf1a1240ed9be3e2500a7caa0297
Changwoo Min <changwoo@igalia.com>
2025-05-28 10:37:19 +0900

    Implement EM_GENL_CMD_PD_GET_TBL.
    Signed-off-by: Changwoo Min <changwoo@igalia.com>

--- em-netlink.c
+++ em-netlink.c
@@ -23,6 +23,14 @@ static const struct nla_policy em_pd_genl_policy[EM_PD_GENL_ATTR_MAX + 1] = {
 	[EM_PD_GENL_ATTR_CPUS]			= { .type = NLA_STRING },
 };
 
+static const struct nla_policy em_tbl_genl_policy[EM_TBL_GENL_ATTR_MAX + 1] = {
+	[EM_TBL_GENL_ATTR_PS_PERFORMANCE]	= { .type = NLA_U64 },
+	[EM_TBL_GENL_ATTR_PS_FREQUENCY]		= { .type = NLA_U64 },
+	[EM_TBL_GENL_ATTR_PS_POWER]		= { .type = NLA_U64 },
+	[EM_TBL_GENL_ATTR_PS_COST]		= { .type = NLA_U64 },
+	[EM_TBL_GENL_ATTR_PS_FLAGS]		= { .type = NLA_U64 },
+};
+
 struct nl_sock *sock;
 int family_id, mc_id;
 
@@ -146,6 +154,89 @@ out_free:
 	return ret;
 }
 
+static int cb_pd_get_tbl(struct nl_msg *msg, void *arg)
+{
+	struct genlmsghdr *gnlh = nlmsg_data(nlmsg_hdr(msg));
+	struct nlattr *attrs[EM_GENL_ATTR_MAX + 1];
+	struct nlattr *tbl_attrs[EM_TBL_GENL_ATTR_MAX + 1];
+	struct nlattr *cur;
+	int i, rem;
+
+	nl_msg_dump(msg, stdout);
+
+	nla_parse(attrs, EM_GENL_ATTR_MAX, genlmsg_attrdata(gnlh, 0),
+		  genlmsg_attrlen(gnlh, 0), NULL);
+
+	if (!attrs[EM_GENL_ATTR_PD_TBL]) {
+		fprintf(stderr, "Missing a mandatory attribute.\n");
+		return NL_SKIP;
+	}
+
+	i = 0;
+	nla_for_each_nested(cur, attrs[EM_GENL_ATTR_PD_TBL], rem) {
+		if (nla_parse_nested(tbl_attrs, EM_TBL_GENL_ATTR_MAX, cur, em_tbl_genl_policy)) {
+			fprintf(stderr, "Failed to parse performance domain information.\n");
+			continue;
+		}
+
+		if (tbl_attrs[EM_TBL_GENL_ATTR_PS_PERFORMANCE])
+			printf("[%d] PERFORMANCE: %lu\n", i, nla_get_u64(tbl_attrs[EM_TBL_GENL_ATTR_PS_PERFORMANCE]));
+		if (tbl_attrs[EM_TBL_GENL_ATTR_PS_FREQUENCY])
+			printf("  [%d] FREQUENCY: %lu\n", i, nla_get_u64(tbl_attrs[EM_TBL_GENL_ATTR_PS_FREQUENCY]));
+		if (tbl_attrs[EM_TBL_GENL_ATTR_PS_POWER])
+			printf("  [%d] POWER: %lu\n", i, nla_get_u64(tbl_attrs[EM_TBL_GENL_ATTR_PS_POWER]));
+		if (tbl_attrs[EM_TBL_GENL_ATTR_PS_COST])
+			printf("  [%d] COST: %lu\n", i, nla_get_u64(tbl_attrs[EM_TBL_GENL_ATTR_PS_COST]));
+		if (tbl_attrs[EM_TBL_GENL_ATTR_PS_FLAGS])
+			printf("  [%d] FLAGS: 0x%lx\n", i, nla_get_u64(tbl_attrs[EM_TBL_GENL_ATTR_PS_FLAGS]));
+
+		i++;
+	}
+
+	return NL_OK;
+}
+
+int cmd_pd_get_tbl(int pd_id)
+{
+	struct nl_msg *msg;
+	int ret;
+
+	msg = nlmsg_alloc();
+	if (!msg) {
+		fprintf(stderr, "Failed to allocate netlink message\n");
+		return 1;
+	}
+
+	genlmsg_put(msg, NL_AUTO_PORT, NL_AUTO_SEQ, family_id, 0, 0,
+		    EM_GENL_CMD_PD_GET_TBL, EM_GENL_VERSION);
+
+	nla_put_u32(msg, EM_PD_GENL_ATTR_ID, pd_id);
+
+	nl_socket_modify_cb(sock, NL_CB_VALID, NL_CB_CUSTOM, cb_pd_get_tbl, NULL);
+
+	ret = nl_send_auto(sock, msg);
+	if (ret < 0) {
+		fprintf(stderr, "Failed to send netlink message\n");
+		goto out_free;
+	}
+
+	ret = nl_wait_for_ack(sock);
+	if (ret < 0) {
+		fprintf(stderr, "Failed to wait for a ACK message\n");
+		goto out_free;
+	}
+
+	ret = nl_recvmsgs_default(sock);
+	if (ret < 0) {
+		fprintf(stderr, "Failed to receive netlink responses: %d\n", ret);
+		goto out_free;
+	}
+
+out_free:
+	nlmsg_free(msg);
+	return ret;
+}
+
 int main(int argc, char *argv[])
 {
 	int ret;
@@ -163,6 +254,18 @@ int main(int argc, char *argv[])
 	if (ret) {
 		fprintf(stderr, "Failed to get pd id\n");
 		goto out_err;
+	}
+
+	/* Request the energy model for each performance domain. */
+	for (int i = 0; i < nr_pd_ids; i++) {
+		int pd_id = pd_ids[i];
+
+		printf("==== Energy model for performance domain %d\n", pd_id);
+		ret = cmd_pd_get_tbl(pd_id);
+		if (ret) {
+			fprintf(stderr, "Failed to get pd id\n");
+			goto out_err;
+		}
 	}
 
 	/* Tear down the netlink connection. */
